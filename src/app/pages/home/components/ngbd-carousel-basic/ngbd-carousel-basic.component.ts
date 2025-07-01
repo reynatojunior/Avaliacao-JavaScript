4877725a7106749b34da7f6eca6cc2db12b8284f
Daniel <danielgilcastro@gmail.com>
2025-07-01 13:49:59 -0300

    nav pronto 2

--- src/app/pages/home/components/ngbd-carousel-basic/ngbd-carousel-basic.component.ts
+++ src/app/pages/home/components/ngbd-carousel-basic/ngbd-carousel-basic.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { ProdutosService } from '../../../../service/produtos.service';
 @Component({
 	selector: 'ngbd-carousel-basic',
 	imports: [NgbCarouselModule],
@@ -8,16 +8,22 @@ import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 })
 export class NgbdCarouselBasic {
 
-	constructor() { }
+	constructor(private produtosService: ProdutosService) { }
 
 	// images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
-	images = ["./img/monitor.png",
-		 "./img/notebook.png",
-		 "./img/phone.png",
-		 "./img/smartphone.png",
-		 "./img/teclado.png",
-		];
+	// images = ["./img/monitor.png",
+	// 	 "./img/notebook.png",
+	// 	 "./img/phone.png",
+	// 	 "./img/smartphone.png",
+	// 	 "./img/teclado.png",
+	produto: any[] = [];
+
+	ngOnInit() {
+		this.produto = this.produtosService.getProdutos();
+		console.log(this.produto[0].imagem);
+	}
+
 
 
 }
--- src/app/service/produtos.service.ts
+++ src/app/service/produtos.service.ts
@@ -8,12 +8,15 @@ export class ProdutosService {
   constructor() {  }
 
   produtos = [
-    { id: 1, produto: 'Notebook', preço: 3500, detalhe: 'Notebook Dell Inspiron 15', imagem: 'notebook.jpg' },
-    { id: 2, produto: 'Smartphone', preço: 2200, detalhe: 'Smartphone Samsung Galaxy S21', imagem: 'smartphone.jpg' },
-    { id: 3, produto: 'Fone de Ouvido', preço: 300, detalhe: 'Fone Bluetooth JBL', imagem: 'fone.jpg' },
-    { id: 4, produto: 'Monitor', preço: 900, detalhe: 'Monitor LG 24"', imagem: 'monitor.jpg' },
-    { id: 5, produto: 'Teclado', preço: 150, detalhe: 'Teclado Mecânico Redragon', imagem: 'teclado.jpg' },
+    { id: 1, produto: 'Notebook', preço: 3500, detalhe: 'Notebook Dell Inspiron 15', imagem: '/img/notebook.png' },
+    { id: 2, produto: 'Smartphone', preço: 2200, detalhe: 'Smartphone Samsung Galaxy S21', imagem: '/img/smartphone.png' },
+    { id: 3, produto: 'Fone de Ouvido', preço: 300, detalhe: 'Fone Bluetooth JBL', imagem: '/img/phone.png' },
+    { id: 4, produto: 'Monitor', preço: 900, detalhe: 'Monitor LG 24"', imagem: '/img/monitor.png' },
+    { id: 5, produto: 'Teclado', preço: 150, detalhe: 'Teclado Mecânico Redragon', imagem: '/img/teclado.png' },
   ];
 
+  getProdutos() {
+    return this.produtos;
+  }
 
 }
