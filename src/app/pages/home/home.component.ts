acb588d0a57eebc28e5602abbc985b82e0678a4a
reynatojunior <reynato.junior@hotmail.com>
2025-07-01 14:11:05 -0300

    commit feat :: contato - sobre

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { NavComponent } from './components/nav/nav.component';
+
 import { NgbdCarouselBasic } from './components/ngbd-carousel-basic/ngbd-carousel-basic.component';
 @Component({
   selector: 'app-home',
-  imports: [NavComponent, NgbdCarouselBasic],
+  imports: [ NgbdCarouselBasic],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
