92d4a0749edb2f069870906243a5cdd079403c28
reynatojunior <reynato.junior@hotmail.com>
2025-07-01 14:33:19 -0300

    commit feat :: footer

--- src/app/pages/contato/contato.component.ts
+++ src/app/pages/contato/contato.component.ts
@@ -1,42 +1,41 @@
-import { Component, OnInit } from '@angular/core'; // Importe OnInit
-import { HttpClient, HttpClientModule } from '@angular/common/http'; // Importe HttpClientModule
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms'; // Mantenha ReactiveFormsModule
+import { Component, OnInit } from '@angular/core'; 
+import { HttpClient, HttpClientModule } from '@angular/common/http'; 
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-contato',
   templateUrl: './contato.component.html',
   styleUrls: ['./contato.component.css'],
-  standalone: true, // Garante que é um componente standalone
+  standalone: true, 
   imports: [
-    ReactiveFormsModule, // Necessário para usar formGroup e formControlName
-    HttpClientModule // Necessário para usar HttpClient
+    ReactiveFormsModule, 
+    HttpClientModule 
   ],
 })
-export class ContatoComponent implements OnInit { // Implemente OnInit
-  contatoForm!: FormGroup; // Declare o FormGroup
+export class ContatoComponent implements OnInit { 
+  contatoForm!: FormGroup; 
 
-  constructor(private fb: FormBuilder, private http: HttpClient) { } // Injete FormBuilder e HttpClient
+  constructor(private fb: FormBuilder, private http: HttpClient) { } 
 
   ngOnInit(): void {
-    // Inicialize o FormGroup aqui, no ngOnInit, pois é o momento certo
-    // para configurar dados de inicialização do componente.
+    
     this.contatoForm = this.fb.group({
       nome: ['', Validators.required],
-      cep: ['', [Validators.required, Validators.pattern(/^[0-9]{8}$/)]], // Adicione validação para o CEP
-      rua: [{ value: '', disabled: true }], // Campos readonly devem ser disabled no FormGroup
+      cep: ['', [Validators.required, Validators.pattern(/^[0-9]{8}$/)]], 
+      rua: [{ value: '', disabled: true }], 
       bairro: [{ value: '', disabled: true }],
       cidade: [{ value: '', disabled: true }],
       estado: [{ value: '', disabled: true }],
       assunto: ['']
     });
 
-    // Subscriba-se às mudanças no campo CEP para acionar a busca
+    
     this.contatoForm.get('cep')?.valueChanges.subscribe(cep => {
-      // Garante que o CEP tem 8 dígitos e é composto apenas por números
+      
       if (cep && cep.length === 8 && /^[0-9]+$/.test(cep)) {
         this.buscarCep(cep);
       } else {
-        // Limpa os campos de endereço se o CEP for inválido ou incompleto
+        
         this.limparEnderecoCampos();
       }
     });
@@ -55,7 +54,7 @@ export class ContatoComponent implements OnInit { // Implemente OnInit
             });
           } else {
             alert('CEP não encontrado.');
-            this.limparEnderecoCampos(); // Limpa se o CEP não for encontrado
+            this.limparEnderecoCampos(); 
           }
         },
         error: (error) => {
@@ -78,17 +77,17 @@ export class ContatoComponent implements OnInit { // Implemente OnInit
   onSubmit(): void {
     if (this.contatoForm.valid) {
       console.log('Formulário enviado:', this.contatoForm.value);
-      // Aqui você pode enviar os dados para seu backend ou fazer outras ações
+      
       alert('Formulário enviado com sucesso!');
-      this.contatoForm.reset(); // Opcional: Limpa o formulário após o envio
+      this.contatoForm.reset(); 
     } else {
       alert('Por favor, preencha todos os campos obrigatórios corretamente.');
-      this.contatoForm.markAllAsTouched(); // Marca todos os campos como "touched" para exibir erros
+      this.contatoForm.markAllAsTouched(); 
     }
   }
 
   onClear(): void {
-    this.contatoForm.reset(); // Limpa o formulário
-    this.limparEnderecoCampos(); // Garante que os campos de endereço também são limpos
+    this.contatoForm.reset(); 
+    this.limparEnderecoCampos(); 
   }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 import { NgbdCarouselBasic } from './components/ngbd-carousel-basic/ngbd-carousel-basic.component';
+import { FooterComponent } from './components/footer/footer.component';
 @Component({
   selector: 'app-home',
-  imports: [ NgbdCarouselBasic],
+  imports: [ NgbdCarouselBasic, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
