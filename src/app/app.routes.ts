95956fa46f2825f5071799cc0e689115b060ec12
Daniel <danielgilcastro@gmail.com>
2025-07-01 10:00:51 -0300

    estrultura base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+    },
+    {
+        path: 'sobre',
+        loadComponent: () => import('./pages/sobre/sobre.component').then(m => m.SobreComponent)
+    },
+    {
+        path: 'contato',
+        loadComponent: () => import('./pages/contato/contato.component').then(m => m.ContatoComponent)
+    }
+];
